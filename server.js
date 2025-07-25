ab49c23ff96cb2ef3ff02d490cb0a3bcc07d4360
lesurdoue <judeonnkot01@gmail.com>
2025-07-25 21:31:08 +0100

    ajout du nouveau dossier au backend

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const sequelize = require('./config');
-const userRoutes = require('./routes/userRoutes');
-
-dotenv.config();
-
 const app = express();
-app.use(cors());
+const userRoutes = require('./routes/userRoutes');
+const sequelize = require('./db');
+require('dotenv').config();
+
 app.use(express.json());
-
-// Connexion à la base de données
-sequelize.authenticate()
-  .then(() => console.log('Connexion à la base de données réussie'))
-  .catch(err => console.error('Erreur de connexion à la base de données :', err));
-
-// Routes principales
 app.use('/api/users', userRoutes);
 
-// Lancer le serveur
 const PORT = process.env.PORT || 5000;
+<<<<<<< HEAD
 app.listen(PORT, () => {
   console.log(`
 Serveur backend lancé sur le port ${PORT}`
 );
 });
+=======
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(Serveur lancé sur le port ${PORT});
+  });
+}).catch(err => {
+  console.error('Erreur de connexion à la base de données:', err);
+});
+>>>>>>> 531a48f (finalisation des ajouts et préparation du pull)
