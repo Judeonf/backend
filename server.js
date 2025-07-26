4ffb2640138841110ac51de6227217a95519e7f5
Judeonf <fabricepartenaire@gmail.com>
2025-07-26 01:18:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
-<<<<<<< HEAD
+ HEAD
 app.listen(PORT, () => {
   console.log(`
 Serveur backend lancé sur le port ${PORT}`
@@ -17,7 +17,7 @@ Serveur backend lancé sur le port ${PORT}`
 
 sequelize.sync().then(() => {
   app.listen(PORT, () => {
-    console.log(Serveur lancé sur le port ${PORT});
+    console.log('Serveur lancé sur le port ${PORT}');
   });
 }).catch(err => {
   console.error('Erreur de connexion à la base de données:', err);
